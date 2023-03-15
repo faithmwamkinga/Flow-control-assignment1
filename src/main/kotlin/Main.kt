471c5ec9d3a63dbb8e21f600a12afc0ca3b6e383
faithmwamkinga <faithmwamkinga@gmail.com>
2023-03-15 13:08:27 +0300

    comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,52 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+numbers()
+    println(namesOfPeople(arrayOf("Joy", "Miriam","Marina","Joune")))
+    party(2)
+    numerals()
 }
+
+fun numbers() {
+    for (number in 1..100) {
+        if (number % 2 == 1) {
+            println(number)
+        }
+    }
+}
+
+fun namesOfPeople(names:Array<String>):Int {
+    var myname = 0
+    for (name in names) {
+        if (name.length > 5) {
+
+         myname++
+        }
+    }
+        return myname
+    }
+
+fun party(age:Int){
+    when{
+      age in  0..5-> println("Glass of milk")
+        age in 6 ..15-> println("Fanta orange")
+        else-> println("Coca-cola")
+    }
+}
+
+fun numerals(){
+    for (numeral in 1..100){
+        if (numeral % 3 == 0 && numeral%5==0) {
+            println("FizzBuzz")
+        }
+         else if (numeral%3==0){
+             println("fizz")
+        }
+        else if (numeral%5==0){
+
+            println("Buzz")
+        }
+        else{
+            println(numeral)
+        }
+    }
+}
+//
